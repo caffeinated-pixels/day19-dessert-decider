22cf3cc27fd49bf682c43ac1238346511f4b2fd0
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-19 10:12:32 -0500

    Add prev imgs to container below main img

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const btn = document.getElementById('btn')
 const foodHolder = document.getElementById('foodHolder')
+const prevDesserts = document.getElementById('prev-desserts')
 btn.addEventListener('click', getDessert)
 
 const apiUrl = 'https://foodish-api.herokuapp.com/api/images/dessert'
+const dessertsArr = []
 
 async function getDessert() {
   try {
     const response = await fetch(apiUrl)
     const data = await response.json()
     displayDessert(data.image)
+    dessertsArr.push(data.image)
+    displayPrevDesserts()
   } catch (error) {
     console.log(error.message)
   }
@@ -17,8 +21,20 @@ async function getDessert() {
 function displayDessert(imgUrl) {
   const imgEl = document.createElement('img')
   imgEl.src = imgUrl
+  imgEl.className = 'large-dessert-img'
   foodHolder.textContent = ''
   foodHolder.appendChild(imgEl)
+}
+
+function displayPrevDesserts() {
+  console.log(dessertsArr)
+  prevDesserts.textContent = ''
+  dessertsArr.forEach((dessert) => {
+    const imgEl = document.createElement('img')
+    imgEl.src = dessert
+    imgEl.className = 'prev-dessert-img'
+    prevDesserts.appendChild(imgEl)
+  })
 }
 
 /* Task:
