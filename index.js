bff123d8beaddec425c3398402cea0b91bed5dba
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-19 07:31:48 -0500

    Setup fetch request

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const btn = document.getElementById('btn')
-// btn.addEventListener("click", findYum)
+btn.addEventListener('click', getDessert)
+
+const apiUrl = 'https://foodish-api.herokuapp.com/api/images/dessert'
+
+async function getDessert() {
+  console.log('click')
+
+  try {
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
 /* Task:
 Call the Foodish API (https://foodish-api.herokuapp.com/) and display random images of desserts on the click of a button.
